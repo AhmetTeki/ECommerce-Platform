47c43ec8adaf4fd23d422b5f64aad469ba93e0b3
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-08-25 17:49:40 +0300

    Mongodb and entities

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities;
+
+public class ProductImages
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string ProductImagesID { get; set; }
+
+    public string Image1 { get; set; }
+
+    public string Image2 { get; set; }
+
+    public string Image3 { get; set; }
+
+    public string ProductId { get; set; }
+    
+    public Product Product { get; set; }
+}
