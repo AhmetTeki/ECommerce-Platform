a883b21ab8837c7c3d354ae906d599f211326c68
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-08-28 17:50:11 +0300

    Controller and configuration proces

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Services.CategoryServices;
+
+namespace MultiShop.Catalog.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoriesController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> CategoryList()
+    {
+        List<ResultCategoryDto> values = await _categoryService.GetAllCategoryAsync();
+        return Ok(values);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCategoryById(string id)
+    {
+        GetByIdCategoryDto value = await _categoryService.GetByIdCategoryAsync(id);
+        return Ok(value);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+    {
+        await _categoryService.CreateCategoryAsync(createCategoryDto);
+        return Ok("Success");
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> DeleteCategory(string id)
+    {
+        await _categoryService.DeleteCategoryAsync(id);
+        return Ok("Success");
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+    {
+        await _categoryService.UpdateCategoryAsync(updateCategoryDto);
+        return Ok("Success");   
+    }
+}
