00b8063ed72955145e559385984eea165ef7e6f8
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-09-07 17:19:40 +0300

    Order repository and context class

--- Services/Order/Infrastructure/MultiShop.Order.Persistance/Context/OrderContext.cs
+++ Services/Order/Infrastructure/MultiShop.Order.Persistance/Context/OrderContext.cs
@@ -1,6 +1,18 @@
-﻿namespace MultiShop.Order.Persistance.Context;
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
 
-public class OrderContext
+namespace MultiShop.Order.Persistance.Context;
+
+public class OrderContext : DbContext
 {
+   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=MultiShopOrderDb;Trusted_Connection=True;");
+    }
+
+    public DbSet<Address> Addresses { get; set; }
     
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    
+    public DbSet<Ordering> Orderings { get; set; }
 }
---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistance/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Persistance.Context;
+
+namespace MultiShop.Order.Persistance.Repositories;
+
+public class Repository<T>: IRepository<T> where T : class
+{
+    private readonly OrderContext _orderContext;
+
+    public Repository(OrderContext orderContext)
+    {
+        _orderContext = orderContext;
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _orderContext.Set<T>().ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _orderContext.Set<T>().FindAsync(id);
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+       await _orderContext.Set<T>().AddAsync(entity);
+       await _orderContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _orderContext.Set<T>().Update(entity);
+        await _orderContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _orderContext.Set<T>().Remove(entity);
+        await _orderContext.SaveChangesAsync();
+    }
+
+    public async Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter)
+    {
+        return await _orderContext.Set<T>().SingleOrDefaultAsync(filter);
+    }
+}
