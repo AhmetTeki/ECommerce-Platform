3c411aa75d60b99202b196d30956b678fb631d21
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-09-10 17:06:31 +0300

    Integrate and implement CQRS handlers for `OrderDetail` and `Address` management, including a new `AddressesController`.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/RemoveOrderDetailCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+
+public class RemoveOrderDetailCommandHandler
+{
+    private readonly IRepository<OrderDetail> _orderDetail;
+
+    public RemoveOrderDetailCommandHandler(IRepository<OrderDetail> orderDetail)
+    {
+        _orderDetail = orderDetail;
+    }
+
+    public async Task Handle(RemoveOrderDetailCommand removeOrderDetailCommand)
+    {
+        OrderDetail value=await _orderDetail.GetByIdAsync(removeOrderDetailCommand.Id);
+        await _orderDetail.DeleteAsync(value);
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/UpdateOrderDetailCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+
+public class UpdateOrderDetailCommandHandler
+{
+    private readonly IRepository<OrderDetail> _orderDetail;
+
+    public UpdateOrderDetailCommandHandler(IRepository<OrderDetail> orderDetail)
+    {
+        _orderDetail = orderDetail;
+    }
+
+    public async Task Handle(UpdateOrderDetailCommand updateOrderDetailCommand)
+    {
+        OrderDetail values = await _orderDetail.GetByIdAsync(updateOrderDetailCommand.OrderDetailId);
+        values.ProductAmount = updateOrderDetailCommand.ProductAmount;
+        values.ProductId = updateOrderDetailCommand.ProductId;
+        values.ProductPrice = updateOrderDetailCommand.ProductPrice;
+        values.ProductName = updateOrderDetailCommand.ProductName;
+        values.OrderingId = updateOrderDetailCommand.OrderingId;
+        values.ProductTotalPrice = updateOrderDetailCommand.ProductTotalPrice;
+        await _orderDetail.UpdateAsync(values);
+    }
+}
