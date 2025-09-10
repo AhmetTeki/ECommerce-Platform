fa5b8f368591d259aed96e169451ac892908d546
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-09-10 17:58:11 +0300

    Add MediatR

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Commands/OrderingCommands/RemoveOrderingRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+
+public class RemoveOrderingRequest: IRequest
+{
+    public int Id { get; set; }
+    
+    public RemoveOrderingRequest(int id)
+    {
+        Id = id;
+    }
+}
