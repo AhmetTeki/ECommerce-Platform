cc2b67c33b4345e07647a5281fdaed409199ca83
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-09-15 16:55:18 +0300

    Register

---
+++ IdentityServer/Multishop.IdentityServer/Controller/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Multishop.IdentityServer.Dtos;
+using Multishop.IdentityServer.Models;
+
+namespace Multishop.IdentityServer.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegisterController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            ApplicationUser values = new ApplicationUser
+            {
+                UserName = userRegisterDto.UserName,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+            };
+            IdentityResult result = await _userManager.CreateAsync(values, userRegisterDto.Password);
+            return Ok(result.Succeeded ? "succes" : "error");
+        }
+    }
+}
