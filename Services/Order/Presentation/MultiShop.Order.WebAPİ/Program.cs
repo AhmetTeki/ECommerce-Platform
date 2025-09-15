c818639614104038e298962e5349403417a76b16
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-09-15 10:09:04 +0300

    MediatR done

--- Services/Order/Presentation/MultiShop.Order.WebAPİ/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebAPİ/Program.cs
@@ -1,8 +1,15 @@
 using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Application.Services;
+using MultiShop.Order.Persistance.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationServices(builder.Configuration);
+
+#region Cqrs
 builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -14,7 +21,7 @@ builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
 builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
-
+#endregion
 
 builder.Services.AddControllers();
 
