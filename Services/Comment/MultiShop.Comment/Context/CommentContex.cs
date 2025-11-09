28c47a51e84b9a99961e3372bee409b0d8696168
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-11-09 18:59:06 +0300

    Add comment management functionality, including migrations, context, entity, project setup, and API integration.

---
+++ Services/Comment/MultiShop.Comment/Context/CommentContex.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Context;
+
+public class CommentContex: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,1437;Database=MultiShopCommentDb;User=sa;Password=Ahmet0234.;");
+    }
+
+    public DbSet<UserComment> UserComments { get; set; }
+}
