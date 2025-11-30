849af13646bf1642f96d62436983130d54766d23
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-11-30 14:41:42 +0300

    Add product review functionality, including new views, partials, controller actions, DTO integration, and API connectivity.

--- Frontend/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontend/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Dto.CommentDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Controllers;
 
 public class ProductListController : Controller
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public ProductListController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
     public IActionResult Index(string CategoryId)
     {
         ViewBag.CategoryId = CategoryId;
@@ -13,6 +23,34 @@ public class ProductListController : Controller
     public IActionResult ProductDetail(string id)
     {
         ViewBag.ProductId = id;
+        return View();
+    }
+
+    [HttpGet]
+    public PartialViewResult AddComment()
+    {
+        return PartialView();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddComment(CreateCommentDto dto)
+    {
+        dto.Status = false;
+        dto.ImageUrl = "test";
+        dto.Rating = 1;
+        dto.CreatedDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+        HttpClient? client = _httpClientFactory.CreateClient();
+        string jsonData = JsonConvert.SerializeObject(dto);
+
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = await client.PostAsync("http://localhost:5168/api/Comments", stringContent);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index", "Default");
+        }
+
         return View();
     }
 }
