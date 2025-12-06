71c32ba4977443fca91dc85b9ac21d34eb9d244a
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-12-06 21:33:17 +0300

    Add login functionality, including views, controllers, DTOs, API integration, and styling.

---
+++ Frontend/MultiShop.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ IdentityServer/Multishop.IdentityServer/Controller/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Multishop.IdentityServer.Dtos;
+using Multishop.IdentityServer.Models;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace Multishop.IdentityServer.Controller
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _userManager;
+
+        public LoginController(SignInManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
+        {
+            SignInResult result = await _userManager.PasswordSignInAsync(userLoginDto.UserName, userLoginDto.Password, false, false);
+            if (result.Succeeded)
+            {
+                return Ok("Succes");
+            }
+            else
+            {
+                return Ok("Failed");
+            }
+        }
+    }
+}
