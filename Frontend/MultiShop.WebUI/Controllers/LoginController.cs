6e06f14cc975cca27ecd5a83a7b5d41892c30305
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-12-06 23:12:13 +0300

    Add login functionality with JWT integration, including controllers, services, DTOs, middleware setup, API connection, and view updates.

--- Frontend/MultiShop.WebUI/Controllers/LoginController.cs
+++ Frontend/MultiShop.WebUI/Controllers/LoginController.cs
@@ -1,11 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Dto.IdentityDtos.LoginDtos;
+using MultiShop.WebUI.Models;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace MultiShop.WebUI.Controllers;
 
 public class LoginController : Controller
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public LoginController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
     public IActionResult Index()
     {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(CreateLoginDto createLoginDto)
+    {
+        HttpClient? client = _httpClientFactory.CreateClient();
+        string content = JsonConvert.SerializeObject(createLoginDto);
+
+        StringContent stringContent = new StringContent(content, Encoding.UTF8, "application/json");
+        HttpResponseMessage response = await client.PostAsync("http://localhost:5001/api/Login", stringContent);
+        if (response.IsSuccessStatusCode)
+        {
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var token = JsonSerializer.Deserialize<JwtResponseModel>(jsonData, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            if (token != null)
+            {
+                JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                var tokenHandler = handler.ReadJwtToken(token.Token);
+                var claims = tokenHandler.Claims.ToList();
+
+                claims.Add(new Claim("multishoptoken", token.Token));
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
+                AuthenticationProperties authProps = new AuthenticationProperties()
+                {
+                    ExpiresUtc = token.ExpireDate,
+                    IsPersistent = true
+                };
+                await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProps);
+                return RedirectToAction("Index", "Default");
+            }
+        }
+
         return View();
     }
 }
--- Frontend/MultiShop.WebUI/Program.cs
+++ Frontend/MultiShop.WebUI/Program.cs
@@ -1,14 +1,28 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MultiShop.WebUI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddCookie(JwtBearerDefaults.AuthenticationScheme, opt =>
+{
+    opt.LoginPath = "/Login/Index";
+    opt.LogoutPath = "/Login/Logout";
+    opt.AccessDeniedPath = "/Pages/AccessDenied";
+    opt.Cookie.HttpOnly = true;
+    opt.Cookie.SameSite = SameSiteMode.Strict;
+    opt.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    opt.Cookie.Name = "MultiShopJwt";
+});
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ILoginService, LoginService>();
+
 builder.Services.AddHttpClient();
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
 
 if (!app.Environment.IsDevelopment())
 {
@@ -18,15 +32,13 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-// AREA ROUTE
 app.MapControllerRoute(
     name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"); 
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-// DEFAULT ROUTE
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
